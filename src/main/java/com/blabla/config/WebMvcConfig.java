0e1a0c1379e70bc153780ea3d76a9b4fb6b6bc7e
hrJin9 <bushyerin97@gmail.com>
2024-05-13 17:09:50 +0900

    build: auth argument resolver 등록

--- src/main/java/com/blabla/config/WebMvcConfig.java
+++ src/main/java/com/blabla/config/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package com.blabla.config;
 
 import com.blabla.config.interceptor.AuthInterceptor;
+import com.blabla.config.resolver.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -13,6 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private static final String[] excludePaths = {"/api/auth/login","/api/auth/register","/api/swagger-ui/**","/api/docs/**", "/api/boards/**", "/api/categories/**"};
 
     private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -28,5 +33,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/static/swagger-ui");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
     }
 }
