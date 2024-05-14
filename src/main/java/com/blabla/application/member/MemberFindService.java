23421cee898f039a3a1cc316a8725a01e5c806a8
hrJin9 <bushyerin97@gmail.com>
2024-05-14 15:20:17 +0900

    refactor: member 서비스 및 레파지토리 auth에서 분리

---
+++ src/main/java/com/blabla/application/member/MemberFindService.java
@@ -0,0 +1,19 @@
+package com.blabla.application.member;
+
+import com.blabla.entity.Member;
+import com.blabla.exception.MemberNotFoundException;
+import com.blabla.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberFindService {
+
+    private final MemberRepository memberRepository;
+
+    public Member findById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 계정입니다."));
+    }
+}
