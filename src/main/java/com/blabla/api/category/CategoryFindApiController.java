6becf2c4ed08ee1dadb4e1e89c7c25bfec943ad9
hrJin9 <bushyerin97@gmail.com>
2024-05-27 17:17:47 +0900

    refactor: 카테고리별 조회 -> 보드에서 분리

--- src/main/java/com/blabla/api/category/CategoryFindApiController.java
+++ src/main/java/com/blabla/api/category/CategoryFindApiController.java
@@ -1,13 +1,14 @@
 package com.blabla.api.category;
 
+import com.blabla.api.board.response.BoardFindResponse;
 import com.blabla.api.category.response.CategoryFindResponse;
+import com.blabla.application.board.BoardFindService;
+import com.blabla.application.board.dto.BoardFindResultDto;
 import com.blabla.application.category.CategoryFindService;
 import com.blabla.application.category.dto.CategoryFindResultDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import java.util.List;
 public class CategoryFindApiController {
 
     private final CategoryFindService categoryFindService;
+    private final BoardFindService boardFindService;
     
     // TODO : 캐싱하기
     @GetMapping
@@ -25,6 +27,21 @@ public class CategoryFindApiController {
         List<CategoryFindResultDto> dtoList = categoryFindService.findCategories();
         List<CategoryFindResponse> response = dtoList.stream()
                 .map(CategoryFindResponse::from)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/{engName}")
+    public ResponseEntity<List<BoardFindResponse>> findBoardsByCategory(
+            @PathVariable String engName,
+            @RequestParam(value = "page-no", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "page-size", defaultValue = "5", required = false) int pageSize,
+            @RequestParam(value = "sort-by", defaultValue = "id", required = false) String sortBy
+    ) {
+
+        List<BoardFindResultDto> dtoList = boardFindService.findBoardsByCategory(pageNo, pageSize, sortBy, engName);
+        List<BoardFindResponse> response = dtoList.stream()
+                .map(BoardFindResponse::from)
                 .toList();
         return ResponseEntity.ok(response);
     }
