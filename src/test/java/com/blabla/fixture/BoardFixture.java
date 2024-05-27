0ed0c632af09671755f4beb912908496eaf347df
hrJin9 <bushyerin97@gmail.com>
2024-05-27 17:18:21 +0900

    refactor: 기존 entity 수정에 따른 테스트 fixture 수정

--- src/test/java/com/blabla/fixture/BoardFixture.java
+++ src/test/java/com/blabla/fixture/BoardFixture.java
@@ -5,17 +5,16 @@ import com.blabla.entity.Board;
 import static com.blabla.fixture.CategoryFixture.CATEGORY1;
 import static com.blabla.fixture.CategoryFixture.CATEGORY2;
 import static com.blabla.fixture.MemberFixture.*;
-import static com.blabla.fixture.TagFixture.TAG1;
 
 public final class BoardFixture {
-    public static final Board BOARD1_CAT1_MEM1 = new Board(1L, "게시판1", "내용", CATEGORY1, MEMBER1,  TAG1 ,false);
-    public static final Board BOARD2_CAT1_MEM2 = new Board(1L, "게시판1", "내용", CATEGORY1, MEMBER2,  TAG1 ,false);
-    public static final Board BOARD3_CAT1_MEM3 = new Board(1L, "게시판1", "내용", CATEGORY1, MEMBER3,  TAG1 ,false);
-    public static final Board BOARD4_CAT2_MEM1_1 = new Board(1L, "게시판1", "내용", CATEGORY2, MEMBER1,  TAG1 ,false);
-    public static final Board BOARD5_CAT2_MEM1_2 = new Board(1L, "게시판1", "내용", CATEGORY2, MEMBER1,  TAG1 ,false);
-    public static final Board BOARD6_CAT2_MEM2 = new Board(1L, "게시판1", "내용", CATEGORY2, MEMBER2,  TAG1 ,false);
-    public static final Board BOARD7_CAT2_MEM3_DEL = new Board(1L, "게시판1", "내용", CATEGORY2, MEMBER3,  TAG1 ,true);
-    public static final Board BOARD8_CAT2_MEM4 = new Board(1L, "게시판1", "내용", CATEGORY2, MEMBER4,  TAG1 ,false);
+    public static final Board BOARD1_CAT1_MEM1 = new Board(1L, "게시판1", "내용", null, 0L, CATEGORY1, MEMBER1 ,null, false);
+    public static final Board BOARD2_CAT1_MEM2 = new Board(2L, "게시판2", "내용", "게시판,태그", 1L, CATEGORY1, MEMBER2, null ,false);
+    public static final Board BOARD3_CAT1_MEM3 = new Board(3L, "게시판3", "내용", "태그", 5L, CATEGORY1, MEMBER3, null,false);
+    public static final Board BOARD4_CAT2_MEM1_1 = new Board(4L, "게시판4", "내용", "태그", 112L, CATEGORY2, MEMBER1,null ,false);
+    public static final Board BOARD5_CAT2_MEM1_2 = new Board(5L, "게시판5", "내용","태그",  3L, CATEGORY2, MEMBER1,null ,false);
+    public static final Board BOARD6_CAT2_MEM2 = new Board(6L, "게시판6", "내용", "태그", 4L, CATEGORY2, MEMBER2,null ,false);
+    public static final Board BOARD7_CAT2_MEM3_DEL = new Board(7L, "게시판7", "내용", "태그", 5L, CATEGORY2, MEMBER3,null ,true);
+    public static final Board BOARD8_CAT2_MEM4 = new Board(8L, "게시판8", "내용", "태그", 10L, CATEGORY2, MEMBER4,null ,false);
 
     public BoardFixture() {
     }
--- src/test/java/com/blabla/fixture/CategoryFixture.java
+++ src/test/java/com/blabla/fixture/CategoryFixture.java
@@ -2,11 +2,14 @@ package com.blabla.fixture;
 
 import com.blabla.entity.Category;
 
+import static com.blabla.fixture.MemberFixture.MEMBER1;
+import static com.blabla.fixture.MemberFixture.MEMBER2;
+
 public final class CategoryFixture {
-    public static final Category CATEGORY1 = new Category(1L, null, "카테고리1", false);
-    public static final Category CATEGORY2 = new Category(2L, null, "카테고리2", false);
-    public static final Category CATEGORY3 = new Category(3L, null, "카테고리3", false);
-    public static final Category CATEGORY4 = new Category(4L, 1L, "카테고리4", false);
+    public static final Category CATEGORY1 = new Category(1L, null, 1L, "카테고리1", "category1", MEMBER1, MEMBER1, false);
+    public static final Category CATEGORY2 = new Category(2L, 1L, 2L, "카테고리2", "category1", MEMBER1, MEMBER1, false);
+    public static final Category CATEGORY3 = new Category(3L, 1L, 3L, "카테고리3", "category1", MEMBER2, MEMBER1, true);
+    public static final Category CATEGORY4 = new Category(4L, 1L, 4L, "카테고리4", "category1", MEMBER2, MEMBER1, false);
 
     public CategoryFixture() {
     }
