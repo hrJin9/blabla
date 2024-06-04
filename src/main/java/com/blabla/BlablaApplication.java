fa643aa32cf3c52a2c3d441f9a137437b260150e
hrJin9 <bushyerin97@gmail.com>
2024-06-04 16:37:04 +0900

    build: ehcache 추가

--- src/main/java/com/blabla/BlablaApplication.java
+++ src/main/java/com/blabla/BlablaApplication.java
@@ -2,8 +2,10 @@ package com.blabla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BlablaApplication {
 
 	public static void main(String[] args) {
