05225927dc95d25c30ac727dc4ecf20db3916f5a
hrJin9 <bushyerin97@gmail.com>
2024-06-04 14:25:20 +0900

    refactor: 공통 로직 분리 및 불필요한 select 쿼리 조정

--- src/main/java/com/blabla/application/board/BoardCommandService.java
+++ src/main/java/com/blabla/application/board/BoardCommandService.java
@@ -2,14 +2,13 @@ package com.blabla.application.board;
 
 import com.blabla.application.board.dto.BoardCreateDto;
 import com.blabla.application.board.dto.BoardUpdateDto;
-import com.blabla.application.category.CategoryFindService;
-import com.blabla.application.member.MemberFindService;
 import com.blabla.entity.*;
 import com.blabla.exception.BoardNotFoundException;
 import com.blabla.exception.BoardUnAuthorizedException;
 import com.blabla.exception.CategoryNotFoundException;
 import com.blabla.repository.board.BoardRepository;
 import com.blabla.repository.category.CategoryRepository;
+import com.blabla.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,17 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BoardCommandService {
 
-    private final MemberFindService memberFindService;
     private final BoardRepository boardRepository;
     private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Long createBoard(Long memberId, BoardCreateDto boardCreateDto) {
 
-        // Board를 생성한 뒤 저장한다.
-        Member member = memberFindService.findById(memberId);
-        Category category = categoryRepository.findById(boardCreateDto.categoryId())
-                .orElseThrow(() -> new CategoryNotFoundException("존재하지 않는 카테고리입니다."));
+        Member member = memberRepository.getReferenceById(memberId);
+        Category category = findByCategoryId(boardCreateDto.categoryId());
 
         Board savedBoard = Board.create(
                 boardCreateDto.subject(),
@@ -45,13 +42,10 @@ public class BoardCommandService {
 
     @Transactional
     public void updateBoard(Long memberId, Long boardId, BoardUpdateDto boardUpdateDto) {
-        Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시글입니다."));
-        if (!board.getWriter().getId().equals(memberId)) {
-            throw new BoardUnAuthorizedException("로그인한 사용자가 작성한 글이 아닙니다.");
-        }
-        Category category = categoryRepository.findById(boardUpdateDto.categoryId())
-                .orElseThrow(() -> new CategoryNotFoundException("존재하지 않는 카테고리입니다."));
+        Board board = findByBoardId(boardId);
+        Category category = findByCategoryId(boardUpdateDto.categoryId());
+
+        isValidMember(board.getWriter().getId(), memberId);
 
         board.update(
                 boardUpdateDto.subject(),
@@ -64,12 +58,27 @@ public class BoardCommandService {
 
     @Transactional
     public void deleteBoardById(Long memberId, Long boardId) {
-        Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시글입니다."));
-        if (!board.getWriter().getId().equals(memberId)) {
-            throw new BoardUnAuthorizedException("로그인한 사용자가 작성한 글이 아닙니다.");
-        }
+        Board board = findByBoardId(boardId);
+
+        isValidMember(board.getWriter().getId(), memberId);
 
         boardRepository.delete(board);
     }
+
+    private void isValidMember(Long loginId, Long writerId) {
+        if (!writerId.equals(loginId)) {
+            throw new BoardUnAuthorizedException("로그인한 사용자가 작성한 글이 아닙니다.");
+        }
+    }
+
+    private Category findByCategoryId(Long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new CategoryNotFoundException("존재하지 않는 카테고리입니다."));
+    }
+
+    private Board findByBoardId(Long boardId) {
+        return boardRepository.findById(boardId)
+                .orElseThrow(() -> new BoardNotFoundException("존재하지 않는 게시글입니다."));
+    }
+
 }
