1590b4e5611052ef1e0352123197099b6f7728a7
hrJin9 <bushyerin97@gmail.com>
2024-06-13 10:40:31 +0900

    docs: swagger 분리

--- src/main/java/com/blabla/config/WebMvcConfig.java
+++ src/main/java/com/blabla/config/WebMvcConfig.java
@@ -46,7 +46,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins("http://blabla-swagger:8080")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
