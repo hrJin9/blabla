ffbb0e9cde5bdacc562d38f2e0e03867e89c2df1
hrJin9 <bushyerin97@gmail.com>
2024-06-13 16:19:56 +0900

    docs: cors orgins 수정

--- src/main/java/com/blabla/config/WebMvcConfig.java
+++ src/main/java/com/blabla/config/WebMvcConfig.java
@@ -46,7 +46,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.209.16.211:8000")
+//                .allowedOrigins("http://13.209.16.211:8000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*");
     }
