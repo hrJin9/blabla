27b7d960f57c9bfc69ed4829593af4d9585252d4
hrJin9 <bushyerin97@gmail.com>
2024-06-17 10:53:46 +0900

    build: cors 설정 수정 - host ip

--- src/main/java/com/blabla/config/WebMvcConfig.java
+++ src/main/java/com/blabla/config/WebMvcConfig.java
@@ -36,7 +36,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://blabla-swagger:8000", "http://blabla-swagger:8080")
+                .allowedOrigins("http://host.docker.internal:8000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
