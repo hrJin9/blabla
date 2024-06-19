5c121ed546528543b313ce23bf228a970ef8d15c
hrJin9 <bushyerin97@gmail.com>
2024-06-19 15:28:39 +0900

    refactor: cors filter 변경

--- src/main/java/com/blabla/config/CorsCustomFilter.java
+++ src/main/java/com/blabla/config/CorsCustomFilter.java
@@ -4,25 +4,19 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 @Slf4j
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsCustomFilter implements Filter {
+public class CorsCustomFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         log.info("CorsCustomFilter.doFilter");
 
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -30,7 +24,8 @@ public class CorsCustomFilter implements Filter {
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
-            chain.doFilter(req, res);
+            chain.doFilter(request, response);
         }
     }
+
 }
