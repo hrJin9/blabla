e3e43db9bec8e1ac467679d72e80881b705e7719
hrJin9 <bushyerin97@gmail.com>
2024-06-19 13:19:07 +0900

    test: 클래스 설정 추가

--- src/test/java/com/blabla/BlablaApplicationTests.java
+++ src/test/java/com/blabla/BlablaApplicationTests.java
@@ -3,7 +3,7 @@ package com.blabla;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BlablaApplicationTests.class)
 class BlablaApplicationTests {
 
 	@Test
